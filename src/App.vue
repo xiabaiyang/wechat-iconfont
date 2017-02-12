962b2518edcd5e0b9a3d336c381b83c9b04a65da
xiabaiyang <baiyangfe@gmail.com>
2017-02-12 23:16:21 +0800

    修改类目名称

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
   <div id="app">
     <div class="router-nav">
       <span class="svg__title">IconFont</span>
-      <router-link to="/svg/mobile" class="router-nav__item">Mobile</router-link>
-      <router-link to="/svg/life" class="router-nav__item">Life</router-link>
-      <router-link to="/svg/game" class="router-nav__item">Game</router-link>
+      <router-link to="/svg/mobile" class="router-nav__item">城市服务</router-link>
+      <router-link to="/svg/life" class="router-nav__item">WEUI</router-link>
+      <router-link to="/svg/game" class="router-nav__item">微信游戏</router-link>
     </div>
     <transition name="fade" mode="out-in">
       <router-view></router-view>
@@ -30,7 +30,7 @@ export default {
 }
 
 .svg__title {
-    margin: 0 100px 0 -175px;
+    margin: 0 50px 0 -175px;
     font-size: 30px;
     height: 30px;
     line-height: 30px;
@@ -45,7 +45,7 @@ export default {
 .router-nav__item {
     display: inline-block;
     text-decoration: none;
-    width: 65px;
+    width: 90px;
     height: 60px;
     margin-right: 30px;
     vertical-align: middle;
